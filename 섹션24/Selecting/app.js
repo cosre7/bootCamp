4295f2dbfe5ffd14bae59aa417499f48190e68a1
cosre7 <cosre7@gmail.com>
2023-12-20 15:05:03 +0900

    섹션24

---
+++ 섹션24/Selecting/app.js
@@ -0,0 +1,7 @@
+// WRITE YOUR CODE IN HERE:
+for (let i = 0; i < 100; i++) {
+    const button = document.createElement('button');
+    button.innerText = 'button';
+    const container = document.querySelector('#container');
+    container.appendChild(button);
+}
