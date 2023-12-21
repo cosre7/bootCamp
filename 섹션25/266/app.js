6aa6669440781f900efe4e99e4fed5f989bdae3e
cosre7 <cosre7@gmail.com>
2023-12-21 10:04:59 +0900

    섹션25: 양식 이벤트 연습

--- 섹션25/266/app.js
+++ 섹션25/266/app.js
@@ -1,12 +1,13 @@
-const form = document.querySelector('#shelterForm');
-const input = document.querySelector('#catName');
-const list = document.querySelector('#cats');
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
+const productInput = document.querySelector('#product');
+const qtyInput = document.querySelector('#qty');
+const list = document.querySelector('#list');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const catName = input.value;
-    const newLI = document.createElement('li');
-    newLI.innerHTML = catName;
-    console.log(newLI);
-    list.append(newLI);
-    input.value = '';
-})
+    const order = document.createElement('li');
+    order.innerText = `${qtyInput.value} ${productInput.value}`;
+    list.appendChild(order);
+    qtyInput.value = '';
+    productInput.value = '';
+});
