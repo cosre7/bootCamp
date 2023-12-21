2c850b2e3cc312c6a1eb99e74437a5cc0b7319be
cosre7 <cosre7@gmail.com>
2023-12-21 09:59:32 +0900

    섹션25: form 실습

--- 섹션25/266/app.js
+++ 섹션25/266/app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('submit');
+    const catName = input.value;
+    const newLI = document.createElement('li');
+    newLI.innerHTML = catName;
+    console.log(newLI);
+    list.append(newLI);
+    input.value = '';
 })
