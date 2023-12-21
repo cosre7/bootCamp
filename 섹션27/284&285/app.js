e4d7a493e62f42ca91ed679900aea91534fdc5de
cosre7 <cosre7@gmail.com>
2023-12-21 16:46:05 +0900

    섹션27: 대기 키워드 await

---
+++ 섹션27/284&285/app.js
@@ -0,0 +1,86 @@
+// async function hello() {
+
+// }
+
+// const sing = async () => {
+//     throw 'oh no';
+//     return 'la la la';
+// }
+
+// sing()
+//     .then(data => {
+//         console.log('promise resolved data :', data)
+//     })
+//     .catch(err => {
+//         console.log('promise rejected data :', err)
+//     })
+
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing Credentials';
+//     if (password === 'corgifeetareute') return 'Welcome!';
+//     throw 'Invalid Password';
+// }
+
+// login('asdfasdf', 'corgifeetareute')
+// .then(msg => {
+//     console.log('logged in')
+//     console.log(msg)
+// })
+// .catch(err => {
+//     console.log('error')
+//     console.log(err)
+// })
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('indigo', 1000))
+//     .then(() => delayedColorChange('violet', 1000))
+
+// async function rainbow() {
+//     await delayedColorChange('red', 1000)
+//     await delayedColorChange('orange', 1000)
+//     await delayedColorChange('yellow', 1000)
+//     await delayedColorChange('green', 1000)
+//     await delayedColorChange('blue', 1000)
+//     await delayedColorChange('indigo', 1000)
+//     await delayedColorChange('violet', 1000)
+//     return 'all done!'
+// }
+
+// rainbow().then(() => console.log('end of rainbow'));
+
+// async function printRainbow() {
+//     await rainbow();
+//     console.log('end of rainbow');
+// }
+
+// printRainbow();
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(');
+            } else {
+                resolve(`Here is your fake data from ${url}`);
+            }
+        }, delay)
+    })
+}
+
+async function makeTwoRequests() {
+    let data1 = await fakeRequest('/page1');
+    console.log(data1); 
+}
