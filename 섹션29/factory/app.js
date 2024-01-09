cbe13355d9f3fc4dacd793fcd7203efbb306e817
cosre7 <cosre7@gmail.com>
2024-01-09 17:32:41 +0900

    refactor : 클래스

--- 섹션29/factory/app.js
+++ 섹션29/factory/app.js
@@ -25,23 +25,53 @@
 
 // // const firstColor = makeColor(35, 255, 150);
 
-function Color(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+// 생성자 함수
+// function Color(r, g, b) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// }
+
+// Color.prototype.rgb = function() {
+//     const {r, g, b} = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// Color.prototype.hex = function() {
+//     const {r, g, b} = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// Color.prototype.rgba = function(a = 1.0) {
+//     const {r, g, b} = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// 클래스
+class Color {
+    constructor(r, g, b, name) {
+        console.log('inside constructor');
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    innerRGB() {
+        const {r, g, b} = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+    rgba(a=1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex() {
+        const {r, g, b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
 }
 
-Color.prototype.rgb = function() {
-    const {r, g, b} = this;
-    return `rgb(${r}, ${g}, ${b})`;
-}
+const red = new Color(255, 0, 0, 'red');
+const white = new Color(255, 255, 255, 'white');
 
-Color.prototype.hex = function() {
-    const {r, g, b} = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-Color.prototype.rgba = function(a = 1.0) {
-    const {r, g, b} = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
