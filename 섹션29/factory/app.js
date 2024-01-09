8e912513c038b5b64ef32ce977a0dd672d80ad22
cosre7 <cosre7@gmail.com>
2024-01-09 17:15:10 +0900

    refactor : 생성자 함수 패턴

--- 섹션29/factory/app.js
+++ 섹션29/factory/app.js
@@ -1,26 +1,47 @@
-function hex(r, g, b) {
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// function hex(r, g, b) {
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// function rgb(r, g, b) {
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// // 객체를 만드는 함수 -> 팩토리 함수 
+// function makeColor(r, g, b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function() {
+//         const {r, g, b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     };
+//     color.hex = function() {
+//         const {r, g, b} = this;
+//         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     return color;
+// }
+
+// // const firstColor = makeColor(35, 255, 150);
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
 }
 
-function rgb(r, g, b) {
+Color.prototype.rgb = function() {
+    const {r, g, b} = this;
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-// 객체를 만드는 함수 -> 팩토리 함수 
-function makeColor(r, g, b) {
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function() {
-        const {r, g, b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    };
-    color.hex = function() {
-        const {r, g, b} = this;
-        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-    return color;
+Color.prototype.hex = function() {
+    const {r, g, b} = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-const firstColor = makeColor(35, 255, 150);
+Color.prototype.rgba = function(a = 1.0) {
+    const {r, g, b} = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
